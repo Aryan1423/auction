0bc12ab740222acaab9920fc1dd44a7d15dd3353
Aryan Singh <113651343+Aryan1423@users.noreply.github.com>
2024-09-10 21:56:09 +0530

    Update not-found.component.jsx

--- src/pages/not-found-page/not-found.component.jsx
+++ src/pages/not-found-page/not-found.component.jsx
@@ -15,7 +15,7 @@ export default function NotFoundPage() {
     useEffect(() => {
         axios({
             method: "get",
-            url: "https://auction-chi-three.vercel.app/user/checkUser",
+            url: "https://backend-ox4t.onrender.com/user/checkUser",
             headers: {
                 "x-access-token": localStorage.getItem("token"),
             },
